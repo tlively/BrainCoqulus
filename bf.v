4b03f94b4871fa4b8ae00d309f9e0ad84722d2ab
Thomas Lively <tlively123@gmail.com>
2017-03-22 20:59:52 -0400

    Implement BF interpreter

--- bf.v
+++ bf.v
@@ -1,6 +1,7 @@
 Require Import ZArith.
 Require FMapList.
 Require Import OrderedType OrderedTypeEx.
+Import ListNotations.
 
 Inductive BF : Set :=
 | bf_eof : BF
@@ -15,3 +16,92 @@ Inductive BF : Set :=
 (* A BFTape is a map from [nat] indices to [Z] values *)
 Module NatMap := FMapList.Make Nat_as_OT.
 Definition BFTape := NatMap.t Z.
+
+Definition tape_get (tape: BFTape) (ptr: nat): Z :=
+  match (NatMap.find ptr tape) with
+  | Some x => x
+  | None => 0
+  end.
+
+Definition tape_set (tape: BFTape) (ptr: nat) (x: Z): BFTape :=
+  NatMap.add ptr x tape.
+
+Definition tape_inc (tape: BFTape) (ptr: nat): BFTape :=
+  tape_set tape ptr (Z.succ (tape_get tape ptr)).
+
+Definition tape_dec (tape: BFTape) (ptr: nat): BFTape :=
+  tape_set tape ptr (Z.pred (tape_get tape ptr)).
+
+(* `bf' is the current state of the program, while `resets' is the
+stack of programs to reset to at the end of a loop *)
+(* TODO: Generalize to all tape languages *)
+Inductive BFState : Type :=
+  bf_state (bf: BF)
+           (resets: list BF)
+           (ptr: nat)
+           (tape: BFTape)
+           (input: list Z)
+           (output: list Z).
+
+Function state_bf (state: BFState): BF :=
+  match state with bf_state bf _ _ _ _ _ => bf end.
+
+Function state_resets (state: BFState): list BF :=
+  match state with bf_state _ resets _ _ _ _ => resets end.
+
+Function state_ptr (state: BFState): nat :=
+  match state with bf_state _ _ ptr _ _ _ => ptr end.
+
+Function state_tape (state: BFState): BFTape :=
+  match state with bf_state _ _ _ tape _ _ => tape end.
+
+Function state_input (state: BFState): list Z :=
+  match state with bf_state _ _ _ _ input _ => input end.
+
+Function state_output (state: BFState): list Z :=
+  match state with bf_state _ _ _ _ _ output => output end.
+
+Function bf_step (state: BFState): option BFState :=
+  match state with
+  | bf_state bf resets ptr tape input output =>
+    match bf with
+     | bf_eof =>
+       match resets with
+        | [] => None
+        | bf' :: resets' =>
+          Some (bf_state bf' resets' ptr tape input output)
+        end
+     | bf_right bf' =>
+       Some (bf_state bf' resets (S ptr) tape input output)
+     | bf_left bf' =>
+       Some (bf_state bf' resets (pred ptr) tape input output)
+     | bf_inc bf' =>
+       Some (bf_state bf' resets ptr (tape_inc tape ptr) input output)
+     | bf_dec bf' =>
+       Some (bf_state bf' resets ptr (tape_dec tape ptr) input output)
+     | bf_out bf' =>
+       Some (bf_state bf' resets ptr tape input (output ++ [tape_get tape ptr]))
+     | bf_in bf' =>
+       match input with
+        | [] =>
+          Some (bf_state bf' resets ptr (tape_set tape ptr 0) input output)
+        | x :: input' =>
+          Some (bf_state bf' resets ptr (tape_set tape ptr x) input' output)
+        end
+     | bf_loop inner_bf bf' =>
+       if Z.eqb (tape_get tape ptr) Z.zero then
+         Some (bf_state bf' resets ptr tape input output)
+       else
+          Some (bf_state inner_bf (bf :: resets) ptr tape input output)
+     end
+  end.
+
+Function bf_run (state: BFState) (fuel: nat): option (list Z) :=
+  match fuel with
+  | 0 => None
+  | S f =>
+    match bf_step state with
+    | None => Some (state_output state)
+    | Some state' => bf_run state' f
+    end
+  end.
