4b26e2e2d1146e70d33f3516143585c9ddfb1572
Thomas Lively <tlively123@gmail.com>
2017-03-24 02:00:58 -0400

    Implement BF AST printing. Break bf.v into sections

--- bf.v
+++ bf.v
@@ -6,6 +6,7 @@ Require FMapList.
 Require Import OrderedType OrderedTypeEx.
 Require Import Strings.String.
 Require Import Strings.Ascii.
+Require Import Coq.Program.Tactics.
 Import ListNotations.
 
 (* BF Program Type *)
@@ -19,103 +20,169 @@ Inductive BF : Set :=
 | bf_in : BF -> BF    (* , *)
 | bf_loop : BF -> BF -> BF.  (* [...] *)
 
-(* TODO: Print AST *)
-(* Function bf_str_state_size (state: BF * list BF): nat := *)
-(*   0. *)
-
-(* Function string_of_bf (state: BF * list BF) *)
-(*          {measure bf_str_state_size state}: string := *)
-(*   let (bf, stack) := state in *)
-(*   match bf with *)
-(*   | bf_end => *)
-(*     match stack with *)
-(*     | [] => EmptyString *)
-(*     | bf' :: stack' => String "]" (string_of_bf (bf', stack')) *)
-(*     end *)
-(*   | bf_right bf' => String ">" (string_of_bf (bf', stack)) *)
-(*   | bf_left bf' => String "<" (string_of_bf (bf', stack)) *)
-(*   | bf_inc bf' => String "+" (string_of_bf (bf', stack)) *)
-(*   | bf_dec bf' => String "-" (string_of_bf (bf', stack)) *)
-(*   | bf_out bf' => String "." (string_of_bf (bf', stack)) *)
-(*   | bf_in bf' => String "," (string_of_bf (bf', stack)) *)
-(*   | bf_loop inner bf' => String "[" (string_of_bf (inner, bf' :: stack)) *)
-(*   end. *)
-
-(* BF Parsing *)
-Inductive ParseState: Type :=
-| parse_ok (str: string) (acc: BF) (stack: list BF)
-| parse_eof (acc: BF) (stack: list BF)
-| parse_error.
-
-Function parse_state_size (state: ParseState): nat :=
-  match state with
-  | parse_ok str _ _  => String.length str
-  | parse_eof _ _ | parse_error => 0
+(* The number of nodes in a BF *)
+Function bf_size (bf: BF): nat :=
+  match bf with
+  | bf_end => 1
+  | bf_right bf'
+  | bf_left bf'
+  | bf_inc bf'
+  | bf_dec bf'
+  | bf_in bf'
+  | bf_out bf' => S (bf_size bf')
+  | bf_loop inner bf' => S (bf_size inner + (bf_size bf'))
   end.
 
-Function bf_parse (state: ParseState)
-         {measure parse_state_size state}: ParseState :=
-  match state with
-  | parse_error
-  | parse_eof _ _ => parse_error
-  | parse_ok str acc stack =>
-    match str with
-    | EmptyString => parse_eof acc stack
-    | String ">" str' => bf_parse (parse_ok str' (bf_right acc) stack)
-    | String "<" str' => bf_parse (parse_ok str' (bf_left acc) stack)
-    | String "+" str' => bf_parse (parse_ok str' (bf_inc acc) stack)
-    | String "-" str' => bf_parse (parse_ok str' (bf_dec acc) stack)
-    | String "." str' => bf_parse (parse_ok str' (bf_out acc) stack)
-    | String "," str' => bf_parse (parse_ok str' (bf_in acc) stack)
-    | String "]" str' => bf_parse (parse_ok str' bf_end (acc :: stack))
-    | String "[" str' =>
+Section BFPrinting.
+  Local Fixpoint bf_str_measure (state: BF * list BF): nat :=
+    let (bf, stack) := state in
+    fold_left (fun acc bf => acc + (bf_size bf)) (bf :: stack) 0.
+
+  Local Lemma lt_a0_lt_fold (bfs: list BF) (a0: nat):
+    fold_left (fun acc bf => acc + (bf_size bf)) bfs a0 <
+    fold_left (fun acc bf => acc + (bf_size bf)) bfs (S a0).
+  Proof.
+    revert a0; induction bfs; auto; intros.
+    replace (a :: bfs) with ([a] ++ bfs) by intuition.
+    rewrite fold_left_app; simpl.
+    auto.
+  Qed.
+
+  Function bf_to_string (state: BF * list BF)
+           {measure bf_str_measure state}: string :=
+    let (bf, stack) := state in
+    match bf with
+    | bf_end =>
       match stack with
-      | [] => parse_error
-      | acc' :: stack' => bf_parse (parse_ok str' (bf_loop acc acc') stack')
+      | [] => EmptyString
+      | bf' :: stack' => String "]" (bf_to_string (bf', stack'))
       end
-    | String _ str' => bf_parse (parse_ok str' acc stack)
-    end
-  end.
-Proof.
-  all: intros; subst; unfold parse_state_size; simpl; auto.
-Defined.
+    | bf_right bf' => String ">" (bf_to_string (bf', stack))
+    | bf_left bf' => String "<" (bf_to_string (bf', stack))
+    | bf_inc bf' => String "+" (bf_to_string (bf', stack))
+    | bf_dec bf' => String "-" (bf_to_string (bf', stack))
+    | bf_out bf' => String "." (bf_to_string (bf', stack))
+    | bf_in bf' => String "," (bf_to_string (bf', stack))
+    | bf_loop inner bf' => String "[" (bf_to_string (inner, (bf' :: stack)))
+    end.
+  Proof.
+    all: intros; subst; simpl; apply lt_a0_lt_fold.
+  Defined.
 
-Function rev_string (acc: string) (str: string): string :=
-  match str with
-  | EmptyString => acc
-  | String c str' => rev_string (String c acc) str'
-  end.
+  Function string_of_bf (bf: BF): string :=
+    bf_to_string (bf, []).
 
-Function parse_bf (bf: string): option BF :=
-  match bf_parse (parse_ok (rev_string EmptyString bf) bf_end []) with
-  | parse_ok _ _ _ | parse_error => None
-  | parse_eof _ (_ :: _) => None
-  | parse_eof ast [] => Some ast
-  end.
+  Example print_all_bf_commands:
+    string_of_bf
+      (bf_loop
+         (bf_right (bf_left (bf_inc (bf_dec (bf_out (bf_in bf_end))))))
+         bf_end)
+    = "[><+-.,]"%string. auto.
+  Qed.
+End BFPrinting.
 
-Example parse_all_bf_commands:
-  parse_bf "[><+-.,]" =
-  Some (bf_loop
-          (bf_right (bf_left (bf_inc (bf_dec (bf_out (bf_in bf_end))))))
-          bf_end).
-auto.
+Section BFParsing.
+  Local Inductive ParseState: Type :=
+  | parse_ok (str: string) (acc: BF) (stack: list BF)
+  | parse_eof (acc: BF) (stack: list BF)
+  | parse_error.
 
-Example parse_nesting_bf:
-  parse_bf "[[[][]]][]" =
-  Some (bf_loop
-          (bf_loop
-             (bf_loop bf_end (bf_loop bf_end bf_end))
-             bf_end)
-          (bf_loop bf_end bf_end)).
-auto.
+  Local Fixpoint parse_state_size (state: ParseState): nat :=
+    match state with
+    | parse_ok str _ _  => String.length str
+    | parse_eof _ _ | parse_error => 0
+    end.
 
-Example parse_bf_bad_left:
-  parse_bf "[[]" = None.
-auto.
+  Function bf_parse (state: ParseState)
+           {measure parse_state_size state}: ParseState :=
+    match state with
+    | parse_error
+    | parse_eof _ _ => parse_error
+    | parse_ok str acc stack =>
+      match str with
+      | EmptyString => parse_eof acc stack
+      | String ">" str' => bf_parse (parse_ok str' (bf_right acc) stack)
+      | String "<" str' => bf_parse (parse_ok str' (bf_left acc) stack)
+      | String "+" str' => bf_parse (parse_ok str' (bf_inc acc) stack)
+      | String "-" str' => bf_parse (parse_ok str' (bf_dec acc) stack)
+      | String "." str' => bf_parse (parse_ok str' (bf_out acc) stack)
+      | String "," str' => bf_parse (parse_ok str' (bf_in acc) stack)
+      | String "]" str' => bf_parse (parse_ok str' bf_end (acc :: stack))
+      | String "[" str' =>
+        match stack with
+        | [] => parse_error
+        | acc' :: stack' => bf_parse (parse_ok str' (bf_loop acc acc') stack')
+        end
+      | String _ str' => bf_parse (parse_ok str' acc stack)
+      end
+    end.
+  Proof.
+    all: intros; subst; unfold parse_state_size; simpl; auto.
+  Defined.
 
-Example parse_bf_bad_right:
-  parse_bf "[]]" = None.
-auto.
+  Function rev_string (acc: string) (str: string): string :=
+    match str with
+    | EmptyString => acc
+    | String c str' => rev_string (String c acc) str'
+    end.
+
+  Function parse_bf (bf: string): option BF :=
+    match bf_parse (parse_ok (rev_string EmptyString bf) bf_end []) with
+    | parse_ok _ _ _ | parse_error => None
+    | parse_eof _ (_ :: _) => None
+    | parse_eof ast [] => Some ast
+    end.
+
+  Example parse_all_bf_commands:
+    parse_bf "[><+-.,]" =
+    Some (bf_loop
+            (bf_right (bf_left (bf_inc (bf_dec (bf_out (bf_in bf_end))))))
+            bf_end).
+  auto. Qed.
+
+  Example parse_nesting_bf:
+    parse_bf "[[[][]]][]" =
+    Some (bf_loop
+            (bf_loop
+               (bf_loop bf_end (bf_loop bf_end bf_end))
+               bf_end)
+            (bf_loop bf_end bf_end)).
+  auto. Qed.
+
+  Example parse_empty_bf:
+    parse_bf EmptyString = Some bf_end.
+  auto. Qed.
+
+  Example parse_bf_bad_left:
+    parse_bf "[[]" = None.
+  auto. Qed.
+
+  Example parse_bf_bad_right:
+    parse_bf "[]]" = None.
+  auto. Qed.
+End BFParsing.
+
+Hint Unfold string_of_bf parse_bf.
+
+(* TODO: Prove this *)
+(* Idea: change parser to build from front instead of back to
+eliminate string reversal *)
+Lemma parse_print_inverse (bf: BF): parse_bf (string_of_bf bf) = Some bf.
+  unfold string_of_bf.
+  induction bf; auto.
+  - rewrite bf_to_string_equation.
+    unfold parse_bf.
+    simpl.
+
+  unfold string_of_bf.
+  unfold parse_bf.
+  remember (bf, []) as state.
+  revert bf Heqstate.
+  functional induction (bf_to_string state); intros; auto.
+  - replace bf with bf_end by congruence; auto.
+  - assert (bf' :: stack' = []) by congruence; discriminate.
+  - replace bf with (bf_right bf') by congruence.
+
 
 (* BF Interpreter *)
 (* A BFTape is a map from [nat] indices to [Z] values *)
