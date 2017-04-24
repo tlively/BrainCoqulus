ba915ea44bc1befdd6eebd9862bd3a3358036492
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2017-04-24 01:40:43 -0400

    Implemented all stack machine steps but rotate

--- sml.v
+++ sml.v
@@ -12,13 +12,12 @@ Module StackMachine.
   Inductive SMProgram : Set :=
   | push : nat -> SMProgram -> SMProgram
   | pop : SMProgram -> SMProgram
-  | get : nat -> SMProgram -> SMProgram. (*
-  | rotate : nat -> SMProgram -> SMProgram
+  | get : nat -> SMProgram -> SMProgram
+  (*| rotate : nat -> SMProgram -> SMProgram *)
   | pack : nat -> SMProgram -> SMProgram
   | unpack : SMProgram -> SMProgram
-  | add : SMProgram -> SMProgram
   | call : SMProgram -> SMProgram
-  | out : SMProgram -> SMProgram. *)
+  | out : SMProgram -> SMProgram.
 
 
   Definition Stack := list Item.
@@ -45,24 +44,73 @@ Module StackMachine.
     | Some a => f a
     end.
 
+  Notation "a >>= f" := (bind a f) (at level 50, left associativity).
+
   Definition tl_error {A} (l : list A) : option (list A) :=
     match l with
     | [] => None
     | _ :: tl => Some tl
     end.
 
-  Function sm_step (s: SMState): option SMState :=
+  Definition hd_error {A} (l : list A) : option (list A) :=
+    match l with
+    | [] => None
+    | hd :: _ => Some hd
+    end.
 
+  Fixpoint stack_pack (n : nat) (s: Stack) :=
+  match n with
+  | 0 => Some s
+  | S 0 => match s with
+    | k :: tl => Some ((item_tuple [k]) :: tl)
+    | _ => None
+    end
+  | S n => (stack_pack n s) >>= (fun s =>
+    match s with
+    | item_tuple l :: k :: tl => Some ((item_tuple (l ++ [k])) :: tl)
+    | _ => None
+    end)
+  end.
+
+  Example stack_pack_simple:
+    stack_pack 2 [item_nat 0; item_nat 1] = Some [item_tuple [item_nat 0; item_nat 1]].
+  simpl. auto.
+  
+
+  Fixpoint stack_unpack (s: Stack) :=
+    match s with
+    | [] => None
+    | hd :: tl => match hd with
+      | item_nat _ => None
+      | item_tuple lst => Some (lst ++ tl)
+      end
+    end.
+
+  Lemma stack_pack_unpack (s s': Stack) (n: nat):
+    n > 0 ->
+    (stack_pack n s) = Some s' ->
+    (stack_pack n s) >>= stack_unpack = Some s.
+  Proof.
+    admit.
+  Admitted.
+
+  Function sm_step (s: SMState): option SMState :=
     match s with
     | state smp fn_table stack input output =>
       let state_from_stack (smp': SMProgram) (stack : Stack) :=
         Some (state smp' fn_table stack input output)
-      in
-      match smp with
+      in match smp with
       | push n smp' => state_from_stack smp' ((item_nat n) :: stack)
-      | pop smp'=> bind (tl_error stack) (state_from_stack smp')
+      | pop smp'=> (tl_error stack) >>= (state_from_stack smp')
       | get n smp' =>
-        let new_stack := bind (nth_error stack n) (fun a => Some (a :: stack)) 
-        in bind new_stack (state_from_stack smp')
+        let new_stack := (nth_error stack n) >>= (fun a => Some (a :: stack)) 
+        in new_stack >>= (state_from_stack smp')
+      | pack n smp' => (stack_pack n stack) >>= (state_from_stack smp')
+      | unpack n smp' => (stack_unpack stack) >>= (state_from_stack smp')
+      | call smp' => match stack with
+        | (item_nat id) :: tl => (nth_error fn_table id) >>= (fun smp =>
+          Some (state smp fn_table tl input output))
+        | _ => None
+        end
       end
     end.
