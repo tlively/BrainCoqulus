7affa20bc59ab6bbb3026ae64a4ab8233af7afee
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2017-05-05 11:52:47 -0400

    next_marked

--- bf.py
+++ bf.py
@@ -104,11 +104,13 @@ class BFMachine:
         # Never halted but got out of fuel
         return
 
-KELL_SIZE = 4
 kl = KELL_SIZE * '<'
 kr = KELL_SIZE * '>'
 
-next_nonzero = (KELL_SIZE - 2) * '>'
+#set_zero = (KELL_SIZE - 3) * '>' + '+' + (KELL_SIZE - 2) * '<'
+unmark = (KELL_SIZE - 2) * '>' + '+' + (KELL_SIZE - 2) * '<'
+mark = (KELL_SIZE - 2) * '>' + '-' + (KELL_SIZE - 2) * '<'
+next_marked = (KELL_SIZE - 2) * '>' + kr + '['+ kr + ']' + (KELL_SIZE - 2) * '<'
 def write_scratch(n): return (KELL_SIZE - 1) * '>' + n * '+' + (KELL_SIZE - 1) * '<'
 
 prev = kl + '[' + kl + ']'
@@ -116,20 +118,21 @@ next = kr + '[' + kr + ']'
 zero_cell = '[-]'
 scc_right = zero_cell + '>[-<+>]<'
 scc_left = zero_cell + '<[->+<]>'
-zero_kell = KELL_SIZE * (zero_cell + '>') + '<<+' + (KELL_SIZE - 2) * '<'
+zero_kell = KELL_SIZE * (zero_cell + '>') + '<<+<+' + (KELL_SIZE - 3) * '<'
 copy_kell = zero_cell + '>' + zero_cell + '>' + scc_right + '[->+<<<+>>]>>>' + scc_left + '[-<+<<<+>>>>]<<<<<'
 zero_item = zero_kell + kr + '[' + zero_kell + kr + ']'
 
 def scc_right_n(n): return (n) * '>' + n * ('<' + scc_right)
-shift_kell = KELL_SIZE * (scc_right_n(KELL_SIZE) + '>') + kl
-sik = shift_kell + 2 * kr + '[' + kl + shift_kell + 2 * kr + ']' + kl + '+' + prev
+shift_kell = KELL_SIZE * (scc_right_n(KELL_SIZE) + '>') + unmark + kl
+sik = shift_kell + 2 * kr + '[' + kl + shift_kell + 2 * kr + ']' + kl + prev
 cik = copy_kell + 2 * kr + '[' + kl + copy_kell + 2 * kr + ']' + kl + copy_kell + prev
 shift_item = next + kl + '[' + sik + kl + ']' + sik
 shift_scratch_left = kl + '[-' + kr + '+' + kl + ']' + kr
 deref = (KELL_SIZE - 1) * '>' + '[' + kr + shift_scratch_left + '-]' + (KELL_SIZE - 1) * '<'
 
 
-def push(n): return kr + '+>' + n * '+' + (KELL_SIZE - 1) * '>'
+
+def push(n): return unmark + kr + unmark + '+>' + n * '+' + (KELL_SIZE - 1) * '>'
 def delete(n): return (n + 1) * prev + n * (shift_item + next) + zero_item
 
 # assumes 'nonzero' and 'zero' will leave me in the same place; cleans up temps.
@@ -149,11 +152,14 @@ def pack(n):
     return prev * n + '>>>' + ('+>>>[+>>>]') * n
 stack_top = kr + '[[' + kr + ']' + kr + ']' + kl
 
+
+shove_zero_gap = '<<[' + (KELL_SIZE-2) * '<' + shift_item + '<<]>>' + kl + shift_item
+
 bfm = BFMachine()
 bfm.bootstrap()
-bfm.run_code(push(3) + push(4) + push(5), 100)
-bfm.run_code(3 * prev, 1500)
-bfm.run_code(write_scratch(5), 1500)
+bfm.run_code(push(3) + push(4) + push(5) + push(6) + '+>++>+>>', 1500)
 bfm.print_state()
-bfm.run_code(deref, 1500)
+bfm.run_code(2 * (prev + kr + mark + kl) + 2 * prev + shift_item, 1500)
+bfm.print_state()
+bfm.run_code(next + mark + next_marked + '[' + kl + shove_zero_gap + kr + unmark + next + mark + next_marked + ']', 4500)
 bfm.print_state()
